5275ed083e7bd9016791bf6a80b204a44943db62
Kian Gorgichuk <kgorgi@uvic.ca>
2017-04-12 23:53:15 -0600

    Implemented Final Velocity Computation and Unit Tests

---
+++ Kinematic Solver for Windows/Kinematic Solver for Windows/Computation/SolveFinVelocity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kinematic_Solver_for_Windows.Exceptions;
+
+namespace Kinematic_Solver_for_Windows
+{
+    public class SolveFinVelocity:KinematicComputer
+    {
+        private double ans;
+        public double CalculateFinVelocity()
+        {
+            if (_D == null)
+            {
+                ans = (this.A * this.T) + this.Vi;
+            }
+            else if (_T == null)
+            {
+                double insideRadical = (this.Vi * this.Vi) + (2 * this.A * this.D);
+                checkRadical(insideRadical);
+                throw new TwoPossibleAnswersException(Math.Sqrt(insideRadical));
+            }
+            else if (_A == null)
+            {
+                ans = ((2 * this.D) / this.T) - this.Vi;
+            }
+            else if (_Vi == null)
+            {
+                ans = (this.D + (0.5 * this.A * (this.T * this.T))) / this.T;
+            } 
+            return ans;
+        }
+    }
+}
---
+++ Kinematic Solver for Windows/KinematicComputerUnitTests/FinVelocityTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kinematic_Solver_for_Windows;
+using Kinematic_Solver_for_Windows.Exceptions;
+
+namespace KinematicComputerUnitTests
+{
+    [TestClass]
+    public class FinVelocityTests
+    {
+        [TestMethod]
+        public void Basic()
+        {
+            IterateVariables(337.5, 5, 15, 30, 105);
+        }
+
+        [TestMethod]
+        public void Decimals()
+        {
+            IterateVariables(46.75, 5.5, 2, 3, 14);
+        }
+
+        [TestMethod]
+        public void Negative()
+        {
+            IterateVariables(-95, 5, -6, -4, -34);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidScenarioException))]
+        public void ImpossibleScenario()
+        {
+            SolveFinVelocity com = InitTest(1, -1, -5, 0.5, 1);
+            com.CalculateFinVelocity();   
+       }
+
+        private static SolveFinVelocity InitTest(double D, double T, double A, double Vi, int skip)
+        {
+            SolveFinVelocity compute = new SolveFinVelocity();
+            if (skip != 0)
+            {
+                compute.D = D;
+            }
+            if (skip != 1)
+            {
+                compute.T = T;
+            }
+            if (skip != 2)
+            {
+                compute.A = A;
+            }
+            if (skip != 3)
+            {
+                compute.Vi = Vi;
+            }
+
+            return compute;
+        }
+
+        private static void IterateVariables(double D, double T, double A, double Vi, double ans)
+        {
+            int i;
+            for (i = 0; i < 4; i++)
+            {
+                SolveFinVelocity com = InitTest(D, T, A, Vi, i);
+
+                try
+                {
+                    Assert.AreEqual(ans, com.CalculateFinVelocity(), "Variable Skipped #: " + i.ToString());
+                }
+                catch (TwoPossibleAnswersException ex)
+                {
+                    Assert.AreEqual(Math.Abs(ans), Math.Abs(ex.ConflictingValue), "Variable Skipped #: " + i.ToString());
+                }   
+            }
+        }
+    }
+}
